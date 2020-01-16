4d97aac4180522f1c24c7dd0ad4104b62888a2a3
curry.zeke <curry.zeke@SEMS-259-17.SEMS259.LAB>
2020-01-15 16:23:13 -0800

    lottery number

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -17,8 +17,37 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+for (int i = 0; i < 10; i++) {
+	
 
-		// 2. Repeat all the code above 10 times
+		if(randomNumber == 0) { 
+	JOptionPane.showMessageDialog(null, "your great");	}
+if(randomNumber == 1) {
+JOptionPane.showMessageDialog(null, "you smell good?");
+}
+if(randomNumber== 2) {		
+JOptionPane.showMessageDialog(null, "your a boomer");
+}
+if(randomNumber == 3) {
+JOptionPane.showMessageDialog(null, "your cool");
+}
+if(randomNumber == 4) {
+JOptionPane.showMessageDialog(null, "you look nice?");
+}if(randomNumber == 5) {
+JOptionPane.showMessageDialog(null, "hey");
+}
+
+}
+
+
+
+
+
+
+
+
+
+// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
---
+++ src/_06_lottery_numbers/lotteryNumbers.java
@@ -0,0 +1,65 @@
+package _06_lottery_numbers;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class lotteryNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+Random randomMaker = new Random();
+		
+		int randomNumber = randomMaker.nextInt(9-1);
+		int randomNumber2 = randomMaker.nextInt(9-1);
+		int randomNumber3 = randomMaker.nextInt(9-1);
+		int randomNumber4 = randomMaker.nextInt(9-1);
+		int randomNumber5 = randomMaker.nextInt(9-1);
+		
+	
+	
+	JOptionPane.showMessageDialog(null, randomNumber+randomNumber2+randomNumber3+randomNumber4+randomNumber5);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
