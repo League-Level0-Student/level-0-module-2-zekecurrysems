5e9263d97775fe67b86dad370e3191eb07705d5b
curry.zeke <curry.zeke@SEMS-259-17.SEMS259.LAB>
2020-02-26 16:23:52 -0800

    callingMethods

--- src/_99_extra/Calling_Methods.java
+++ src/_99_extra/Calling_Methods.java
@@ -4,58 +4,35 @@ public class Calling_Methods {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int num;
 
-
-	
-        System.out.println("message");
-	
-	
-        this.name = name;
-	
-	
-        return number;
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-		 void printMessage() {
-		 }
-             
-             void setName(String name) {
-
-             } 	 	
-            
-             int getNumber() {
-
-        }
+	setName("joe");
+		
+		
+		printMessage();
+num =getNumber();	
+		
+		
+	}	
+		static void printMessage() {
+            System.out.println("message");
+   } 	
+		
+		 static void setName(String name) {
+             name = name;
+    } 	
+		
+		 static int getNumber() {
+             return 0; 
+		 }	
+		
+		
+		
 	
 	
 	
 	
 	
 
-}
-	
+
+}	
